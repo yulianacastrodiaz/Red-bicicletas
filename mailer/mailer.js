463135aa62536bcc5427076775856c55052bdb68
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-06-02 09:33:33 -0500

    Agregamos sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,37 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email.',
-    port: 587,
-    auth: {
-        user: 'jairo.kirlin@ethereal.email',
-        pass: 'KMjMHvAz2pBx5aHr8s'
+let mailConfig;
+if (process.env.NODE_ENV === 'production'){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+
+} else {
+    if (process.env.NODE_ENV === 'staging'){
+        console.log('XXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(this.options);
+
+    } else {
+        // all emails are catched by ethereal.email
+        mailConfig = {
+            host: 'smtp.ethereal.email.',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process-env.ethereal_pwd
+            }
+        } 
+    }
+    
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
